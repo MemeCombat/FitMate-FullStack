7d4845c09d91e22d58d6c98571e75d50b4abf3f5
ernestngenest <ernestprabowo@gmail.com>
2024-10-09 16:03:00 +0700

    update product

--- src/app/fitting/page.jsx
+++ src/app/fitting/page.jsx
@@ -118,7 +118,7 @@ const Fitting = () => {
       if (response.ok) {
         const result = await response.json();
         console.log("result: ini result  ", result);
-        router.push(`/result?ProductId=${result.createdPhoto.insertedId}`);
+        router.push(`/result?productId=${result.createdPhoto.insertedId}`);
       } else {
         console.error("Error generating photo");
       }
