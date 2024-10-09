0f270cf1ac0f10a4be727707b4aa1fff1c67e195
ernestngenest <ernestprabowo@gmail.com>
2024-10-09 15:33:56 +0700

    fix:shop

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,12 +1,47 @@
-
 "use client";
-import React from "react";
-import Button from "../components/Button";
+import React, { useState } from "react";
+import Select from "react-select";
+import Dropdown from "./Dropdown";
+import Modal from "./Modal";
 
+const ProductCard = ({ product, index }) => {
+  const [isModalActive, setIsModalActive] = useState(false);
+  const [selectedTags, setSelectedTags] = useState([]);
 
+  const cardColors = [
+    "bg-yellow-300",
+    "bg-blue-300",
+    "bg-green-300",
+    "bg-red-300",
+    "bg-purple-300",
+  ];
 
-const PricingCard = ({ type, price, description, isPopular, token, color }) => {
-  const backgroundColor = getBackgroundColor(type, color);
+  const items = [
+    {
+      name: "Delete",
+      link: "",
+    },
+    {
+      name: "Update",
+      link: "/update",
+    },
+  ];
+
+  const tagOptions = [
+    { value: "casual", label: "Casual" },
+    { value: "formal", label: "Formal" },
+    { value: "streetwear", label: "Streetwear" },
+    { value: "vintage", label: "Vintage" },
+    { value: "bohemian", label: "Bohemian" },
+    { value: "minimalist", label: "Minimalist" },
+    { value: "athleisure", label: "Athleisure" },
+    { value: "preppy", label: "Preppy" },
+  ];
+
+  const handleTagChange = (selectedOptions) => {
+    setSelectedTags(selectedOptions);
+  };
+
   return (
     <div
       key={index}
@@ -192,45 +227,4 @@ const PricingCard = ({ type, price, description, isPopular, token, color }) => {
   );
 };
 
-const getBackgroundColor = (type, color) => {
-  switch (type.toUpperCase()) {
-    case "GOLD":
-      return "bg-yellow-400";
-    case "SILVER":
-      return "bg-gray-400";
-    case "BRONZE":
-      return "bg-yellow-700";
-    default:
-      return color;
-  }
-};
-
-const PopularBadge = () => (
-  <div className="bg-black text-white font-bold py-1 px-4 absolute top-0 right-0">
-    Popular
-  </div>
-);
-
-const PricingCardHeader = ({ type }) => (
-  <h2 className="text-4xl font-black text-black mb-2">{type}</h2>
-);
-
-const PricingCardBody = ({ price, token, description }) => (
-  <div>
-    <p className="text-5xl font-black text-black mb-4">{price}</p>
-    <p className="text-xl text-black mb-6 font-bold">
-      get {token} token for fitting
-    </p>
-    <p className="text-xl text-black mb-6 font-bold">{description}</p>
-    <ul className="space-y-3 text-black mb-8"></ul>
-  </div>
-);
-
-const PricingCardFooter = () => (
-  <div className="mt-auto">
-    <Button className="w-full justify-center">Click here to buy</Button>
-  </div>
-);
-
-
-export default PricingCard;
+export default ProductCard;
